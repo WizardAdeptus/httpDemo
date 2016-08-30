d535dc3a543990e5744cf33ae30b820fe70c9c68
WizardAdeptus <wizard.adeptus@ya.ru>
2016-08-30 18:49:53 +0400

    Initial commit

---
+++ src/com/mysoft/Main.java
@@ -0,0 +1,10 @@
+package com.mysoft;
+
+/**
+ * Created by Игорь on 30.08.2016.
+ */
+public class Main {
+    public static String test() {
+        return "Это код из класса Java";
+    }
+}
---
+++ src/com/mysoft/servlet/MyServlet.java
@@ -0,0 +1,17 @@
+package com.mysoft.servlet;
+
+import java.io.IOException;
+
+/**
+ * Created by Игорь on 30.08.2016.
+ */
+@javax.servlet.annotation.WebServlet(name = "MyServlet")
+public class MyServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
